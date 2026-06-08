73ece615ddf38bb0144d8b04fb2657ce44d85cb3
Brian Inca <brianincah10@gmail.com>
2026-06-08 09:32:20 -0500

    feat: ci/cd configuration and Azure

--- rrhh-frontend/src/environments/environment.prod.ts
+++ rrhh-frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     HOST: 'https://app-backend-rrhh.victorioussand-e9c1bf78.centralus.azurecontainerapps.io/api',
     keycloak: {
-        url: 'https://rrhh-keycloak-identity.victorioussand-e9c1bf78.centralus.azurecontainerapps.io',
+        url: 'https://rrhh-keycloak-identity.victorioussand-e9c1bf78.centralus.azurecontainerapps.io/auth',
         realm: 'rrhh-core',
         clientId: 'rrhh-ui'
     }
