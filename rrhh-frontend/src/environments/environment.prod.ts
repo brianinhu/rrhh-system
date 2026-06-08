43857eed26dab08314e785f41d8e6417b86dd7e3
Brian Inca <brianincah10@gmail.com>
2026-06-08 11:57:27 -0500

    fix: remove /auth for url keycloak

--- rrhh-frontend/src/environments/environment.prod.ts
+++ rrhh-frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     HOST: 'https://app-backend-rrhh.victorioussand-e9c1bf78.centralus.azurecontainerapps.io/api',
     keycloak: {
-        url: 'https://rrhh-keycloak-identity.victorioussand-e9c1bf78.centralus.azurecontainerapps.io/auth',
+        url: 'https://rrhh-keycloak-identity.victorioussand-e9c1bf78.centralus.azurecontainerapps.io',
         realm: 'rrhh-core',
         clientId: 'rrhh-ui'
     }
