af1bc6cb483fb701fd6ab6f728011034df388ea8
Brian Inca <brianincah10@gmail.com>
2026-06-15 12:38:35 -0500

    fix: remove /auth from keycloak url demo

--- rrhh-frontend/src/environments/environment.demo.ts
+++ rrhh-frontend/src/environments/environment.demo.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     HOST: 'http://localhost:8080/api',
     keycloak: {
-        url: 'http://localhost:8180/auth',
+        url: 'http://localhost:8180',
         realm: 'rrhh-core',
         clientId: 'rrhh-ui'
     }
